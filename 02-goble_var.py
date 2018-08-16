090a439054dfca59903f3f6f6be4afb559176dba
zhangchuan013 <zhangchuan013@126.com>
2018-08-16 09:09:24 +0800

    备注

---
+++ 02-goble_var.py
@@ -0,0 +1,13 @@
+#函数的定义以及全局局部变量的使用
+get_name = input("please input name:")
+get_age = int(input("please input age:"))
+def print_base_info():
+	global get_age
+	get_age = 10
+	print("the one name is %s age %d"%(get_name,get_age))
+
+def print_old_inf():
+	get_age = 10
+	print("the second name is %s age %d"%(get_name,get_age))
+print_base_info()
+print_old_inf()
